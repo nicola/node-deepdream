74dd38cd2919baf953d4ad2e8fb33dcc4096bac4
nicola <notsecurity@gmail.com>
2015-07-10 16:29:18 +0200

    updating the README

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # node-deepdream
+
+** Attention: ** this is a very silly experiment.
+
+## Design idea
+
+The idea is to run a Python server and connect to it through Node.js. There is no intention in rewriting the code in javascript since it would be incredibly less performant.
+
+## Licence
+
+MIT
--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 var debug = require('debug')('deepdream:error')
 
+function deepdream (opts) {
+  opts = opts || {}
+  this.net = opts.net
+  this.img = opts.img
+  this.iter = opts.iter || 10
+  this.octave = opts.octave || 4
+  this.step = opts.step || 1.5
+  this.octaveScale = this.octaveScale || 1.4
+  this.jitter = opts.jitter || 32
+  this.end = opts.end || 'inception_4c/output'
+  this.clip = clip || true
+}
 debug('Work in progress')
 
